3be129effde27547aedef838162d4403240cf19f
Marlon <martianworm17@gmail.com>
2017-10-14 17:22:47 +0800

    Python Tuples

---
+++ variables/python_tuple.py
@@ -0,0 +1,9 @@
+tuplez    = ('abcd', 786, 2.23, 'john', 70.2)
+tinytuple = (123, 'daniel')
+
+print tuplez        # Prints complete tuple
+print tuplez[0]     # Prints first element of the tuple
+print tuplez[1:3]   # Prints elements starting from 2nd till 3rd
+print tuplez[2:]    # Prints elements starting from 3rd element
+print tinytuple * 2 # Prints tuple two times
+print tuplez + tinytuple    # Prints concatenated tuples 
