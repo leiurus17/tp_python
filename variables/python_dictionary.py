ac056d564eb9a8a906fb9744997555e1e7f04ae0
Marlon <martianworm17@gmail.com>
2017-10-14 18:24:25 +0800

    Change name dict to dictz

--- variables/python_dictionary.py
+++ variables/python_dictionary.py
@@ -1,11 +1,11 @@
-dict = {}
-dict['one'] = "This is one"
-dict[2]     = "This is two"
+dictz = {}
+dictz['one'] = "This is one"
+dictz[2]     = "This is two"
 
 tinydict = {'name':'john', 'code':6734, 'dept':'sales', 2:4}
 
-print dict['one']       # Prints value for 'one' key
-print dict[2]           # Prints value for 2 key
+print dictz['one']       # Prints value for 'one' key
+print dictz[2]           # Prints value for 2 key
 print tinydict[2]
 print tinydict          # Prints complete dictionary
 print tinydict.keys()   # Prints all the keys
