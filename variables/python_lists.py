6f5d87383749f956b75c10e546392c5aed341632
Marlon <martianworm17@gmail.com>
2017-10-14 17:03:38 +0800

    Python Lists

---
+++ variables/python_lists.py
@@ -0,0 +1,9 @@
+listz    = ['abcd', 768, 2.23, "John Travolta", 70.2]
+tinylist = [123, 'daniel']
+
+print listz         # Prints complete list
+print listz[0]      # Prints first element of the list
+print listz[1:3]    # Prints elements starting from 2nd till 3rd
+print listz[2:]     # Prints elements starting from 3rd element
+print tinylist * 2  # Prints list two times
+print listz + tinylist # Prints concatenated lists
