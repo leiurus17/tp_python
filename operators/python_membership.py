4a62ef0764e1697b662b3726df18bd7839268d1e
Marlon <martianworm17@gmail.com>
2017-10-31 19:49:16 +0800

    Python Membership Operators Example

---
+++ operators/python_membership.py
@@ -0,0 +1,21 @@
+a = 10
+b = 20
+
+list = [1, 2, 3, 4, 5]
+
+if (a in list):
+    print "a is in the list"
+else:
+    print "a is not in list"
+
+if (b not in list):
+    print "b is not in list"
+else:
+    print "b is in list"
+    
+a = 2
+
+if (a in list):
+    print "a is in the list"
+else:
+    print "a is not in the list"
