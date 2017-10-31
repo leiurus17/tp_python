5bea0de387169e37bd916052207c4fcd94dacc6b
Marlon <martianworm17@gmail.com>
2017-10-31 18:26:04 +0800

    Python Assignment Operators Example

---
+++ operators/python_assignment.py
@@ -0,0 +1,32 @@
+a = 21
+b = 10
+c = 0
+
+print "a = ", a
+print "b = ", b
+print "c = ", c
+
+c = a + b
+print "c = a + b is ", c
+
+c += a
+print "c += a is ", c
+
+c *= a
+print "c *= a is ", c
+
+c /= a
+print "c /= a is ", c
+
+c = 2
+
+print "c = ", c
+
+c %= a
+print "c %= a is ", c
+
+c **= a
+print "c **= a is ", c
+
+c //= a
+print "c //= a is ", c
