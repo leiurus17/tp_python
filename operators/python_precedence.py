067ed7bfcd9fa4aa85fb9a7e2d6f6c4dee791463
Marlon <martianworm17@gmail.com>
2017-10-31 20:15:56 +0800

    Python Operators Precedence Example

---
+++ operators/python_precedence.py
@@ -0,0 +1,17 @@
+a = 20
+b = 10
+c = 15
+d = 5
+e = 0
+
+e = (a + b) * c / d
+print "e = (a + b) * c / d is ", e
+
+e = ((a + b) * c) / d
+print "e = ((a + b) * c) / d is ", e
+
+e = (a + b) * (c / d)
+print "e = (a + b) * (c / d) is ", e
+
+e = a + (b * c) / d
+print "e = (a + b) * (c / d) is ", e
