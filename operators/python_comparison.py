8efd7e0e64c6c53c12f26269970377fa6bde24cf
Marlon <martianworm17@gmail.com>
2017-10-31 18:00:44 +0800

    Python Comparison Operators Example

---
+++ operators/python_comparison.py
@@ -0,0 +1,48 @@
+a = 21
+b = 10
+c = 0
+
+print a
+print b
+print c
+
+if (a == b):
+    print "a == b is True"
+else:
+    print "a == b is False"
+    
+if (a != b):
+    print "a != b is True"
+else:
+    print "a != b is False"
+    
+if (a <> b):
+    print "a <> b is True"
+else:
+    print "a <> b is False"
+    
+if (a < b):
+    print "a < b is True"
+else:
+    print "a < b is False"
+    
+if (a > b):
+    print "a > b is True"
+else:
+    print "a > b is False"
+    
+a = 5
+b = 20
+
+print a
+print b
+
+if (a <= b):
+    print "a <= b is True"
+else:
+    print "a <= b is False"
+
+if (b >= a):
+    print "b >= a is True"
+else:
+    print "b >= a is False"
