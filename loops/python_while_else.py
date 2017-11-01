c587163b3216c864359c429de0ad6aa7f9218390
Marlon <martianworm17@gmail.com>
2017-11-01 17:17:55 +0800

    Python while Loop Statements

---
+++ loops/python_while_else.py
@@ -0,0 +1,6 @@
+count = 0
+while count < 10:
+    print count, " is less than 10"
+    count = count + 1
+else:
+    print count, "is not less than 10"
---
+++ python_while_single_statement.py
@@ -0,0 +1,4 @@
+flag = 1
+# Infinite loop incoming
+while (flag): print 'Given flag is really true!'
+print "Good bye!"
