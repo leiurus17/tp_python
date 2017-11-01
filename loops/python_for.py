d83cdd1a920c20129f93e54f8fbf50de7b672431
Marlon <martianworm17@gmail.com>
2017-11-01 18:15:06 +0800

    Python for Loop Statements

---
+++ loops/python_for.py
@@ -0,0 +1,9 @@
+for letter in 'Python':
+    print 'Current letter: ', letter
+
+fruits = ['banana', 'apple', 'mango']
+
+for fruit in fruits:
+    print 'Current fruit: ', fruit
+    
+print "Good bye!"
