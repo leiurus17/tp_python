249b65850735ae6f82f5c132f9a5cfae5d0d64e5
Marlon <martianworm17@gmail.com>
2017-11-01 21:46:50 +0800

    Python continue statement

---
+++ loops/python_continue.py
@@ -0,0 +1,15 @@
+for letter in 'Python':     # First Example
+    if letter == 'h':
+        continue
+    print 'Current Letter: ', letter
+    
+var = 10                    # Second example
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    
+    
+    print "Current variable value: ", var
+
+print "Good bye!"
