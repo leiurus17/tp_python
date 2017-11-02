02d6d7e26144644d8e40d552db0885253ca68748
Marlon <martianworm17@gmail.com>
2017-11-02 22:42:30 +0800

    Python Number log() Method

---
+++ numbers/python_log.py
@@ -0,0 +1,6 @@
+import math
+
+print "math.log(100.12): ", math.log(100.12)
+print "math.log(100.72): ", math.log(100.72)
+print "math.log(119L): ",   math.log(119L)
+print "math.log(math.pi): ", math.log(math.pi)
