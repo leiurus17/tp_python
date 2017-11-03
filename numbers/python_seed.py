6a4034375dba2af074d9f898fcaae5df1f9cfc73
Marlon <martianworm17@gmail.com>
2017-11-03 19:26:41 +0800

    Python Number seed() Method

---
+++ numbers/python_seed.py
@@ -0,0 +1,13 @@
+import random
+
+random.seed(10)
+print "Random number with seed 10: ", random.random()
+
+random.seed(10)
+print "Random number with seed 10: ", random.random()
+
+random.seed(10)
+print "Random number with seed 10: ", random.random()
+
+random.seed(4)
+print "Random number with seed 4: ", random.random()
