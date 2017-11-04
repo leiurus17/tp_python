73ed6fea17372124b900658b7da18e66d06afeac
Marlon <martianworm17@gmail.com>
2017-11-04 20:13:54 +0800

    Python String count() Method

---
+++ strings/python_count.py
@@ -0,0 +1,9 @@
+str = "this is string example....wow!!!"
+print str
+
+sub = "i"
+print sub
+print "str.count(sub, 0, 40):", str.count(sub, 0, 40)
+sub = "wow"
+print sub
+print "str.count(sub): ", str.count(sub)
