ee448f2ec8b6ade81ca0a26c6dcf362b27d14066
Marlon <martianworm17@gmail.com>
2017-11-04 10:00:42 +0800

    Python Number tan() Method

---
+++ numbers/python_trig_tan.py
@@ -0,0 +1,8 @@
+import math
+
+print "tan(3): ", math.tan(3)
+print "tan(-3): ", math.tan(-3)
+print "tan(0): ", math.tan(0)
+print "tan(math.pi): ", math.tan(math.pi)
+print "tan(math.pi/2): ", math.tan(math.pi/2)
+print "tan(math.pi/4): ", math.tan(math.pi/4)
