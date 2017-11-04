54c2769cd1badded85c053882f9de9f8566bd76d
Marlon <martianworm17@gmail.com>
2017-11-04 10:02:36 +0800

    Python Number degrees() Method

---
+++ numbers/python_trig_degrees.py
@@ -0,0 +1,8 @@
+import math
+
+print "degrees(3): ", math.degrees(3)
+print "degrees(-3): ", math.degrees(-3)
+print "degrees(0): ", math.degrees(0)
+print "degrees(math.pi): ", math.degrees(math.pi)
+print "degrees(math.pi/2): ", math.degrees(math.pi/2)
+print "degrees(math.pi/4): ", math.degrees(math.pi/4)
