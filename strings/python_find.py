8c329ed724e20de0d958d4ee094b507349016518
Marlon <martianworm17@gmail.com>
2017-11-04 20:56:55 +0800

    Python String find() Method

---
+++ strings/python_find.py
@@ -0,0 +1,9 @@
+# str.find(str, beg=0, end=len(string))
+
+str1 = "this is string example...wow!!!"
+str2 = "exam"
+
+print str1.find(str2)
+print str1.find(str2, 10)
+print str1.find(str2, 40)
+print str1.find(str2, 0, 40)
