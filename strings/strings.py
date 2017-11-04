60c6bcc841d8de8be8282011574cb1cd1d5fd383
Marlon <martianworm17@gmail.com>
2017-11-04 10:55:08 +0800

    Accessing Values in Strings

---
+++ strings/strings.py
@@ -0,0 +1,14 @@
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print var1
+print var2
+
+print "var1[0]", var1[0]
+print "var2[1:5]", var2[1:5]
+
+var1 = 'Hello World!'
+print "Updated String :-", var1[:6] + 'Python'
+print var1
+var3 = var1[:6] + 'Python'
+print var3
