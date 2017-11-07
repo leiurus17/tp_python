f486520b114ff08e8eb6c61d19e1bde25c3069bc
Marlon <martianworm17@gmail.com>
2017-11-07 19:08:40 +0800

    Fix missing '8' parameter

--- strings/python_lstrip.py
+++ strings/python_lstrip.py
@@ -2,4 +2,4 @@ str = "                This is string example.                   "
 print str.lstrip()
 
 str = "8888888888888888This is string example.8888888888888888888"
-print str.lstrip()
+print str.lstrip('8')
