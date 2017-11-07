be4e00e389713d685775b780de06cbf987ee1259
Marlon <martianworm17@gmail.com>
2017-11-07 19:31:10 +0800

    Python String rfind() Method

---
+++ strings/python_rfind.py
@@ -0,0 +1,10 @@
+str1 = "This is really a string example...wow!!!"
+str2 = "is"
+
+print str1.rfind(str2)
+print str1.rfind(str2, 0, 10)
+print str1.rfind(str2, 10, 0)
+
+print str1.find(str2)
+print str1.find(str2, 0, 10)
+print str1.find(str2, 10, 0)
