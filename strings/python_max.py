6c1168cb8e1a63db6d07cf2dcc8f74eea6e3e12e
Marlon <martianworm17@gmail.com>
2017-11-07 19:17:07 +0800

    Python String max() Method

---
+++ strings/python_max.py
@@ -0,0 +1,7 @@
+#The method max() returns the max alphabetical character from the string str.
+
+str = "This is really a string example....wow!!! z"
+print "Max character: " + max(str)
+
+str = "This is a string example."
+print "Max character: " + max(str)
