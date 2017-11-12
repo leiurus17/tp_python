ee424497fc6ded3f902c3a70a1a4422e6ccf34e7
Marlon <martianworm17@gmail.com>
2017-11-12 19:03:23 +0800

    Basic Tuples Operations

--- tuples/python_tuples.py
+++ tuples/python_tuples.py
@@ -22,4 +22,12 @@ tup2 = ('abc', 'xyz')
 tup3 = tup1 + tup2
 print tup3
 del tup3
-print tup3
+#print tup3
+
+print len(tup1)
+print (1, 2, 3) + (4, 5, 6)
+print ('Hi!',) * 4
+print 3 in (1, 2, 3)
+print 4 in (1, 2, 3)
+
+for x in (1, 2, 3) : print x
