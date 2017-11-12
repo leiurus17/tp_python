9576452a3785317471ac3ecacae9d8cfb53757d2
Marlon <martianworm17@gmail.com>
2017-11-12 22:52:39 +0800

    Updating Dictionary

--- dictionary/python_dictionaries.py
+++ dictionary/python_dictionaries.py
@@ -4,3 +4,11 @@ print "dictionary['Name']: ", dictionary['Name']
 print "dictionary['Age']: ", dictionary['Age']
 print "dictionary['Class']: ", dictionary['Class']
 #print "dictionary['Sagala']: ", dictionary['Sagala']
+
+#Update a dictionary
+dictionary['Age']   = 12
+dictionary['Class'] = 'Hogwarts'
+
+print "dictionary['Name']: ", dictionary['Name']
+print "dictionary['Age']: ", dictionary['Age']
+print "dictionary['Class']: ", dictionary['Class']
