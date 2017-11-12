bb985e9623ee2ef2d1be195257816c834d0676fd
Marlon <martianworm17@gmail.com>
2017-11-12 22:50:55 +0800

    Accessing Values in Dictionary

---
+++ dictionary/python_dictionaries.py
@@ -0,0 +1,6 @@
+dictionary = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+print "dictionary['Name']: ", dictionary['Name']
+print "dictionary['Age']: ", dictionary['Age']
+print "dictionary['Class']: ", dictionary['Class']
+#print "dictionary['Sagala']: ", dictionary['Sagala']
