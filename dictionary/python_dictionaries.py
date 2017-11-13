1f12a507d5295f410789d6ed381a1aa3fffc911c
Marlon <martianworm17@gmail.com>
2017-11-13 11:13:25 +0800

    Delete Dictionary Elements

--- dictionary/python_dictionaries.py
+++ dictionary/python_dictionaries.py
@@ -12,3 +12,12 @@ dictionary['Class'] = 'Hogwarts'
 print "dictionary['Name']: ", dictionary['Name']
 print "dictionary['Age']: ", dictionary['Age']
 print "dictionary['Class']: ", dictionary['Class']
+
+student = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print student
+del student['Name']
+print student
+student.clear()
+print student
+#del student
+#print student
