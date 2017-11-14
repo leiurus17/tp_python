eeba11700641a7d574322944710ac98d62cbc6fe
Marlon <martianworm17@gmail.com>
2017-11-14 19:10:27 +0800

    Keyword arguments

---
+++ functions/function_keyword_args.py
@@ -0,0 +1,8 @@
+def printme(string, number):
+    "This prints a passed string into this function"
+    print string
+    print number
+    return
+
+printme(string = "Uzumaki Boruto dattebasa!", number = 21)
+printme(number = 13, string = "Hey, I called this function before. :)")
