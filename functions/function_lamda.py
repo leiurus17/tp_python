798a43f616ae25f5902c510ec7a13cf938a96d56
Marlon <martianworm17@gmail.com>
2017-11-14 20:22:53 +0800

    The Anonymous Functions

--- functions/function_lamda.py
+++ functions/function_lamda.py
@@ -1,6 +1,6 @@
 # Function definition is here
-sum = lambda arg1, arg2: arg1 + arg2
+summation = lambda arg1, arg2: arg1 + arg2
 
 # Now you can call sum as a function
-print "Value of total: ", sum(10, 20)
-print "Value of total: ", sum(20, 20)
+print "Value of total: ", summation(10, 20)
+print "Value of total: ", summation(20, 20)
