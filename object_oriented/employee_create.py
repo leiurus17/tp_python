a8773850e58a9bc436a88b34e4ea2816478e7294
Marlon <martianworm17@gmail.com>
2017-11-15 17:03:28 +0800

    Accessing Attributes

---
+++ object_oriented/employee_create.py
@@ -0,0 +1,11 @@
+from Employee import Employee
+
+emp1 = Employee("Ava", 2000)
+
+emp2 = Employee("Emma", 3000)
+
+print Employee.empCount
+print emp1.name
+print emp2.name
+print emp1.displayEmployee()
+print emp2.displayEmployee()
